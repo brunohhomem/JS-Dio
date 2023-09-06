8df45221cb28a7546d132f98e818323fbde6e419
Bruno H. Homem <brunohhomem@gmail.com>
2023-09-06 11:57:57 -0300

    Aula 02

---
+++ Aula 02 - Variaveis/script.js
@@ -0,0 +1,60 @@
+// //Tipos primitivos
+
+// //Boolean
+// var vOuF = false
+// console.log(vOuF)
+// console.log(typeof vOuF)
+
+// //Number
+// var numeroQualquer = 1
+// console.log(numeroQualquer)
+// console.log(typeof numeroQualquer)
+
+// //String
+// var textoQualquer = 'teste de texto'
+// console.log(textoQualquer)
+// console.log(typeof textoQualquer)
+
+// //se declarar uma variavel mas n definir um valor, ela ficara como "undefined"
+
+// //Atribuição
+// var atribuicao = 'bruno'
+// console.log(atribuicao)
+
+// //comparacao
+// var comparacao = '0' == 0
+// console.log(comparacao)
+
+// //comparacao identica (valor e tipo de dado)
+// var comparacaoIdentitica = '0' === 0
+// console.log(comparacaoIdentitica)
+
+// //adicao
+// var adicao = 1 + 1
+
+// //subtracao
+// var subtracao = 1 - 1
+
+// //divisao
+// var divisao = 1 / 1
+
+// //multiplicacao
+// var multiplicacao = 1 * 1
+
+// //divisao inteira
+// var divisaoInteira = 5 % 2
+
+// //potenciacao
+// var potenciacao = 2 ** 10
+
+// //maior que
+// var maiorQue = 5 > 2
+
+// //menor que
+// var menorQue = 5 < 2
+
+// //maior ou igual a
+// var maiorOuIgualA = 5 >= 5;
+
+// //menor ou igual a
+// var menorOuIgualA = 1 <= 10;
